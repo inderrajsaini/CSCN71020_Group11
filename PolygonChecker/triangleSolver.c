35afda827edbc9567f790567db1dce368a825d4d
Drashti Bhaveshkumar Patel <drashti12102000@gmail.com>
2021-11-07 13:54:35 -0500

    calculateIntAnglesOfTriangle

--- PolygonChecker/triangleSolver.c
+++ PolygonChecker/triangleSolver.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include "triangleSolver.h"
-
+#include<math.h>
+#define pi 3.141592
 
 char* analyzeTriangle(int side1, int side2, int side3) {
 	char* result = "";
@@ -30,4 +31,24 @@ bool triangleInequalityTheorem(int s1, int s2, int s3)
 		isTriangle = true;
 	return isTriangle;
 
+}
+
+void calculateIntAnglesOfTriangle(int s1, int s2, int s3)
+{
+	double s1s2Angle, s2s3Angle, s3s1Angle;
+	s1s2Angle = calcAngle(s1, s2, s3);
+	s2s3Angle = calcAngle(s2, s3, s1);
+	s3s1Angle = 180 - (s1s2Angle + s2s3Angle);
+	printf("The angles of the triangle are: %lf, %lf, %lf degrees.\n", s1s2Angle, s2s3Angle, s3s1Angle);
+}
+
+double calcAngle(int adjSide1, int adjSide2, int oppSide)
+{
+	double angle = (double)(adjSide1 * adjSide1 + adjSide2 * adjSide2 - oppSide * oppSide) / (2 * adjSide1 * adjSide2);
+	return convertRadToPi(acos(angle));
+}
+
+double convertRadToPi(double radians)
+{
+	return radians * (180 / pi);
 }
--- PolygonChecker/triangleSolver.h
+++ PolygonChecker/triangleSolver.h
@@ -1,3 +1,6 @@
 #pragma once
 char* analyzeTriangle(int side1, int side2, int side3);
 bool triangleInequalityTheorem(int s1, int s2, int s3);
+void calculateIntAnglesOfTriangle(int s1, int s2, int s3);
+double calcAngle(int adjSide1, int adjSide2, int oppSide);
+double convertRadToPi(double radians);
