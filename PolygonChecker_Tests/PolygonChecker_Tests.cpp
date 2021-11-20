35af30afedb9094d8e85cb20236ecac60fc1a039
inderrajsaini <inderraj.r.saini27@gmail.com>
2021-11-21 01:02:27 +0530

    Some tests for rectangle written

--- PolygonChecker_Tests/PolygonChecker_Tests.cpp
+++ PolygonChecker_Tests/PolygonChecker_Tests.cpp
@@ -4,6 +4,8 @@ extern "C" char* analyzeTriangle(int side1, int side2, int side3);
 extern "C" bool triangleInequalityTheorem(int s1, int s2, int s3);
 extern "C" double calcAngle(int adjSide1, int adjSide2, int oppSide);
 extern "C" bool analyzeFourPoints(int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4, int* perimeter, int* area);
+extern "C" int findDistBw2Pts(int x1, int y1, int x2, int y2);
+extern "C" bool isSquare(int sqOfDiagonal, int sqOfSide);
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace PolygonCheckerTests
@@ -154,6 +156,66 @@ namespace PolygonCheckerTests
 			actual = calcAngle(s1, s2, s3);
 
 			Assert::AreEqual(EXPECTED, actual);
+		}
+	};
+	TEST_CLASS(AnalyzeFourPointsTests)
+	{
+	public:
+		TEST_METHOD(Test1_1_findDistBw2Pts_Three_One_TwoandFour_Ten)
+		{
+			int x1 = 3;
+			int y1 = 1;
+			int x2 = 2;
+			int y2 = 4;
+			int EXPECTED = 10;
+			int actual;
+
+			actual = findDistBw2Pts(x1, y1, x2, y2);
+
+			Assert::AreEqual(EXPECTED, actual);
+
+		}
+		TEST_METHOD(Test1_2_findDistBw2Pts_negativeThree_One_TwoandFour_ThirtyFour)
+		{
+			int x1 = -3;
+			int y1 = 1;
+			int x2 = 2;
+			int y2 = 4;
+			int EXPECTED = 34;
+			int actual;
+
+			actual = findDistBw2Pts(x1, y1, x2, y2);
+
+			Assert::AreEqual(EXPECTED, actual);
+
+		}
+		TEST_METHOD(Test1_3_findDistBw2Pts_Zero_Onea_TwondFour_Thirteen)
+		{
+			int x1 = 0;
+			int y1 = 1;
+			int x2 = 2;
+			int y2 = 4;
+			int EXPECTED = 13;
+			int actual;
+
+			actual = findDistBw2Pts(x1, y1, x2, y2);
+
+			Assert::AreEqual(EXPECTED, actual);
+
+		}
+		TEST_METHOD(Test2_1_isSquare_Three_One_TwoandFour_Ten)
+		{
+			int x1 = 3;
+			int y1 = 1;
+			int x2 = 2;
+			int y2 = 4;
+			int EXPECTED = 10;
+			int actual;
+
+			actual = findDistBw2Pts(x1, y1, x2, y2);
+
+			Assert::AreEqual(EXPECTED, actual);
+
 		}
 	};
 }
